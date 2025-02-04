8182ccb8c15943ad7c16fbb5ad61acd3c7bd208b
Athot <athotmangkung@gmail.com>
2025-02-04 17:05:37 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/prayagedu/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.prayagedu
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color header = Color(0xFF1B1A55);
+  static const Color grey = Color(0xFFD9D9D9);
+  static const Color bottomBar = Color(0xFF535C91);
+  static const Color hover = Color(0xFF9290C3);
+  static const Color red = Colors.red;
+  static const Color green = Colors.green;
+  static const Color search = Color(0xFFEFE4E4);
+  static const Color notiCorr = Color(0xFFDDE6ED);
+  static const Color notiWrong = Color(0xFF9DB2BF);
+}
